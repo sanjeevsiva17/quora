088bd671c99f078d40e72d79a27b216056ce2318
sanjeevsiva17 <sanjeevsiva17@gmail.com>
2020-03-24 10:38:15 +0530

    answer functionality

--- dashboard/views.py
+++ dashboard/views.py
@@ -3,7 +3,8 @@ from django.shortcuts import render, get_object_or_404, redirect
 # Create your views here.
 from django.http import HttpResponse
 from .models import Question, Answer
-from .forms import  QuestionForm, AnswerForm
+from .forms import QuestionForm, AnswerForm
+
 
 def index(request):
     questions = Question.objects.all()
@@ -16,8 +17,9 @@ def question(request, question_id):
     answer = Answer.objects.filter(question=question)
     return render(request, 'question.html', {"question": question, "answers": answer})
 
+
 def add_question(request):
-    if request.method=="POST":
+    if request.method == "POST":
         form = QuestionForm(request.POST)
         if form.is_valid():
             post = form.save(commit=False)
@@ -28,3 +30,17 @@ def add_question(request):
         form = QuestionForm()
     context = {'form': form}
     return render(request, 'question_form.html', context)
+
+
+def add_answer(request):
+    if request.method == "POST":
+        form = AnswerForm(request.POST)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.user = request.user
+            post.save()
+            return redirect('index')
+    else:
+        form = AnswerForm()
+    context = {'form': form}
+    return render(request, 'answer_form.html', context)
