68e42e6b63e066d72e1ac49fec8db0967cc6ef09
sanjeevsiva17 <sanjeevsiva17@gmail.com>
2020-03-25 10:45:20 +0530

    added basic css to all pages

--- dashboard/forms.py
+++ dashboard/forms.py
@@ -5,8 +5,14 @@ class QuestionForm(forms.ModelForm):
     class Meta:
         model = Question
         fields= ['question_text',]
+        widgets = {
+          'question_text': forms.Textarea(attrs={'rows':20, 'cols':100}),
+        }
 
 class AnswerForm(forms.ModelForm):
     class Meta:
         model = Answer
         fields=['answer_text',]
+        widgets = {
+          'answer_text': forms.Textarea(attrs={'rows':20, 'cols':100}),
+        }
